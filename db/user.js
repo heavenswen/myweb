9ba36fdcb7a69f033810044a2caa44493a27e658
Heavenswen <423822728@qq.com>
2017-09-11 17:59:04 +0800

    post get all

--- db/user.js
+++ db/user.js
@@ -1,7 +1,10 @@
-//mysql 查询
-var UserSQL = {  
+//mysql 查询 
+var UserSQL = {
+    //模版语句
     insert:'INSERT INTO User(uid,userName) VALUES(?,?)', 
+    //查询 user
     queryAll:'SELECT * FROM User',  
+    //获得uid
     getUserById:'SELECT * FROM User WHERE uid = ? ',
   };
   
--- routes/db.js
+++ routes/db.js
@@ -3,11 +3,15 @@ var express = require('express');
 var router = express.Router();
 // 导入MySQL模块
 var mysql = require('mysql');
+//db 配置
 var dbConfig = require('../db/config');
+//查询语句
 var userSQL = require('../db/user');
+
 // 使用DBConfig.js的配置信息创建一个MySQL连接池
 var pool = mysql.createPool(dbConfig.mysql);
-// 响应一个JSON数据
+
+// 响应JSON数据
 var responseJSON = function (res, ret) {
     if (typeof ret === 'undefined') {
         res.json({
@@ -17,9 +21,31 @@ var responseJSON = function (res, ret) {
         res.json(ret);
     }
 };
+
 //底层路由
-router.get('/',function(req,res,next){
-    res.send('11');
+router.get('/', function (req, res, next) {
+
+    // 从连接池获取连接 
+    pool.getConnection(function (err, connection) {
+        // 建立连接 增加一个用户信息 
+        connection.query(userSQL.queryAll, [param.uid, param.name], function (err, result) {
+            if (result) {
+                result = {
+                    code: 200,
+                    msg: '查询成功'
+                };
+            }
+
+            // 以json形式，把操作结果返回给前台页面     
+            responseJSON(res, result);
+
+            // 释放连接  
+            connection.release();
+
+        });
+    })
+
+
 })
 // 添加用户
 router.get('/addUser', function (req, res, next) {
