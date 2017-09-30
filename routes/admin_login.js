a6f7ddaced6bfa4d1fd96c902d6d00573497bac3
Heavenswen <423822728@qq.com>
2017-09-30 18:05:24 +0800

    login

--- routes/admin_login.js
+++ routes/admin_login.js
@@ -30,12 +30,16 @@ router.get('/', function (req, res, next) {
 //登录验证
 router.post("/login", function (req, res, next) {
     var parem = req.body;
+    //获得 验证码
     var vcode = req.sesstion;
+    
+    //登录俩次失败进行验证
+    if (vcode && vcode.loginState > 2 && !parem.vcode) res.send({ code: -200, msg: '验证码不能为空' });
 
-    if (vcode && vcode.loginState > 2 && !parem.vcode) res.send({ code: -200, msg: '用户名不能为空' });
     //查询 1条
     knex.select('*').from('Users').where({ name: parem.user }).limit(1).asCallback(function (err, rows) {
         if (err) { next(err) }
+        //是否存在
         if (rows.length) {
             //用户存在
             let pw = parem.pw + config.pw
@@ -46,16 +50,21 @@ router.post("/login", function (req, res, next) {
             //对比密码
             if (end_paw === get_pw) {
 
+
                 res.send({ code: 200, msg: '密码正确' })
+
             } else {
+                //添加登录失败次数
+                req.sesstion.loginState++;
+
                 res.send({ code: -200, msg: '密码或用户名错误' })
 
             }
 
-        }else{
+        } else {
             //用户不存在
             res.send({ code: -200, msg: '用户名不存在' })
-            
+
         }
     })
 
